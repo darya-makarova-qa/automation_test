8160c285794c3ed88d463911cee6c86fdb116fa3
Darya <mdara4257@gmail.com>
2026-02-15 13:59:03 +0200

    Fix test

--- tests/test_navigation_ii.py
+++ tests/test_navigation_ii.py
@@ -6,6 +6,6 @@ def test_products_button_opens_products_page(page):
     products_link = page.get_by_role("link", name="Products") # Находим ссылку "Products" (тег <a>)
     expect(products_link).to_be_visible() # Проверяем что ссылка видна пользователю
     products_link.click() # Нажимаем на ссылку "Products"
-    expect(page).to_have_url("https://automationexercise.com/WRONG") # Проверяем, что после клика мы перешли на страницу /products
+    expect(page).to_have_url("https://automationexercise.com/products") # Проверяем, что после клика мы перешли на страницу /products
     expect(page.get_by_text("All Products")).to_be_visible     # Проверяем что на странице есть заголовок All Products
     expect(page.get_by_text("View Product").first).to_be_visible() # Проверяем что товары отображаются и есть кнопка "View Product"
